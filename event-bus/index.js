364f332da3944dea2522d2fe3e7dee0d20388bfc
Samet Palitci <hyorax@gmail.com>
2021-01-21 19:48:34 +0300

    InÄitial Commit

---
+++ event-bus/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+app.use(express.json());
+
+app.post('/events', async (req, res) => {
+    const event = req.body;
+
+    await axios.post('http://localhost:4000/events', event);
+    await axios.post('http://localhost:4001/events', event);
+
+    res.send({ status: "ok" })
+});
+
+app.listen(4005, () => {
+    console.log('App listen to 4005');
+});
---
+++ posts/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
+
+const axios = require('axios');
+
+const app = express();
+
+app.use(cors())
+
+const posts = {};
+
+app.use(express.json());
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/posts', async (req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const { title } = req.body;
+
+    posts[id] = {
+        id, title
+    }
+
+    await axios.post('http://localhost:4005/events', {
+        type: 'PostCreated',
+        data: {
+            id,
+            title
+        }
+    });
+
+    res.status(201).send(posts[id]);
+})
+
+
+app.listen(4000, () => {
+    console.log("Listening on 4000");
+})
