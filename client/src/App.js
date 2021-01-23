d653f49f809a100b2503456316a8884ced64a22a
Samet Palitci <hyorax@gmail.com>
2021-01-23 10:13:43 +0300

    Finish Project

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+
 import PostCreate from './PostCreate';
 import PostList from './PostList';
+
 const App = (props) => {
     return (
         <div className="container">
--- event-bus/index.js
+++ event-bus/index.js
@@ -7,9 +7,9 @@ app.use(express.json());
 app.post('/events', async (req, res) => {
     const event = req.body;
 
-    await axios.post('http://localhost:4000/events', event);
-    await axios.post('http://localhost:4001/events', event);
-    await axios.post('http://localhost:4002/events', event);
+     axios.post('http://localhost:4000/events', event);
+     axios.post('http://localhost:4001/events', event);
+     axios.post('http://localhost:4002/events', event);
 
     res.send({ status: "ok" })
 });
